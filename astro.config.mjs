373df925e2f786293ab8cf86f84e98aabaecee49
Shailen Mishra <35340754+shailenmishra@users.noreply.github.com>
2026-08-12 01:47:33 -0400

    Add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import { fontProviders } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   fonts: [
@@ -19,7 +21,10 @@ export default defineConfig({
       fallbacks: ["system-ui", "sans-serif"],
     },
   ],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
